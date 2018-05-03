0ec6bf2a9b5a1a4195cf1c920c49b4322268668e
Benjamin Koppe <Benjamin.Koppe@gmx.de>
2018-05-04 00:05:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+A simple webapp to ease brochure printing
+
+Administering church services I sometimes need to print leaflets with hymn lyrics and/or details on the liturgy.
+
+Usually it's printed in A4 papers being folded to hold the contents on A5 pages.
+
+I'd write all down in proper order, select 2 pages on one paper and duplex long edge in the printer properties.
+
+Then I'd give the order for the pages to be printed in. This usually involves a lot of thinking and calculating.
+
+So I made this webapp. You enter the number of your pages (which needs to be a multiple of 4) and get the order in which the pages need to be printed so you have all neatly on foldable A4 sheets to make an A5 brochure.
+
+You can easily copy the string to your clipboard to paste it into your printer's page order text field.
---
+++ js/index.js
@@ -0,0 +1,49 @@
+function additional(x){
+  switch (x%4){
+    case 1:
+      return 3;
+    case 2:        
+      return 2;
+    case 3:
+      return 1;
+    case 0:
+      return 0;
+  }
+};
+
+$("document").ready(function(){
+  document.getElementById('submit').onclick = function(){
+    pagenumber = document.getElementById('pagenumber').value;
+    output="You need further "+additional(pagenumber)+" blank pages.";
+    pagenumber= Number(pagenumber) + Number(additional(pagenumber));
+    times = pagenumber/4;
+    counter = 0;
+    resultstring="";
+    for (i=0;i<times;i++){
+      resultstring+=pagenumber+",";
+      pagenumber--;
+      counter++;
+      resultstring+=counter+",";
+      counter++;
+      resultstring+=counter+",";
+      resultstring+=pagenumber+",";
+      pagenumber--;
+    }
+    resultstring=resultstring.slice(0,resultstring.length-1);
+    //output+=resultstring;
+    $('#output').html(output);
+    document.getElementById('copystring').value=resultstring;
+    $('#success').html('');
+  }
+  document.getElementById('copybutton').onclick = function(){
+    document.getElementById('copystring').focus();
+    document.getElementById('copystring').select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      $('#success').html('Copying text command was ' + msg);
+    } catch (err) {
+      $('#success').html('Oops, unable to copy');
+    }
+  }
+});
