a621edad3defaef59a0aeeb03eb3d56dbf4dd7ef
tonnerkiller <Benjamin.Koppe@gmx.de>
2023-12-21 22:15:37 +0100

    Getting rid of more jquery

--- js/index.js
+++ js/index.js
@@ -11,39 +11,41 @@ function additional(x){
   }
 };
 
-$("document").ready(function(){
-  document.getElementById('submit').onclick = function(){
-    pagenumber = document.getElementById('pagenumber').value;
-    output="You need "+additional(pagenumber)+" more blank pages at the end of your document.";
-    pagenumber= Number(pagenumber) + Number(additional(pagenumber));
-    times = pagenumber/4;
-    counter = 0;
-    resultstring="";
-    for (i=0;i<times;i++){
-      resultstring+=pagenumber+",";
-      pagenumber--;
-      counter++;
-      resultstring+=counter+",";
-      counter++;
-      resultstring+=counter+",";
-      resultstring+=pagenumber+",";
-      pagenumber--;
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+
+    document.getElementById('submit').onclick = function(){
+      pagenumber = document.getElementById('pagenumber').value;
+      output="You need "+additional(pagenumber)+" more blank pages at the end of your document.";
+      pagenumber= Number(pagenumber) + Number(additional(pagenumber));
+      times = pagenumber/4;
+      counter = 0;
+      resultstring="";
+      for (i=0;i<times;i++){
+        resultstring+=pagenumber+",";
+        pagenumber--;
+        counter++;
+        resultstring+=counter+",";
+        counter++;
+        resultstring+=counter+",";
+        resultstring+=pagenumber+",";
+        pagenumber--;
+      }
+      resultstring=resultstring.slice(0,resultstring.length-1);
+      document.getElementById('output').setHTML(output);
+      document.getElementById('copystring').value=resultstring;
+      document.getElementById('success').setHTML('');
     }
-    resultstring=resultstring.slice(0,resultstring.length-1);
-    //output+=resultstring;
-    document.getElementById('output').setHTML(output);
-    document.getElementById('copystring').value=resultstring;
-    document.getElementById('success').setHTML('');
-  }
-  document.getElementById('copybutton').onclick = function(){
-    document.getElementById('copystring').focus();
-    document.getElementById('copystring').select();
-    try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'successful' : 'unsuccessful';
-      document.getElementById('success').setHTML('Copying text command was ' + msg);
-    } catch (err) {
-      document.getElementById('success').setHTML('Oops, unable to copy');
+    document.getElementById('copybutton').onclick = function(){
+      document.getElementById('copystring').focus();
+      document.getElementById('copystring').select();
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        document.getElementById('success').setHTML('Copying text command was ' + msg);
+      } catch (err) {
+        document.getElementById('success').setHTML('Oops, unable to copy');
+      }
     }
   }
-});
+};
